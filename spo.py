3292b4a8053ec483c6fbf922ca9b6ca20cf05fc1
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2022-10-19 10:51:21 +0200

    added methode to retreive access token

--- spo.py
+++ spo.py
@@ -7,7 +7,8 @@ class SharePointHelper():
     _client_secret = None
     _tenant_id = None
     _base_url = None
-    _token_url = "https://accounts.accesscontrol.windows.net/"
+    _oauth2_token = None
+    _token_url = "https://accounts.accesscontrol.windows.net"
     _default_principal_audience_id = "00000003–0000–0ff1-ce00–000000000000"
     
     def __init__(self, tenant, _client_id, _client_secret, _tenant_id):
@@ -16,31 +17,43 @@ class SharePointHelper():
         self._client_secret = _client_secret
         self._tenant_id = _tenant_id
         self._base_url = f"https://www.{self._tenant}.sharepoint.com/"
-        self._base = f"{self._tenant}.sharepoint.com"
+        self._base = f"{tenant}.sharepoint.com"
     
     def get_oauth2_token(self):
-        token_url = f"{self._token_url}/{self._tenant_id}/tokens/oauth/2"
-        resource = f"{self._default_principal_audience_id}/{self._base}@{self._tenant_id}"
+        spo_token_url = f"https://accounts.accesscontrol.windows.net/{self._tenant_id}/tokens/oauth/2"
+        default_principal_id = "00000003-0000-0ff1-ce00-000000000000"
+        resource = f"{default_principal_id}/{self._tenant}@{self._tenant_id}"
         client_id = f"{self._client_id}@{self._tenant_id}"
-        token = None
         headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
+            'Content-Type' : 'application/x-www-form-urlencoded'
         }
         body = {
-            "grant_type": "client_credentials",
-            "client_id": client_id,
-            "client_secret": self._client_secret,
-            "resource": resource
+            "grant_type" : "client_credentials",
+            "client_id" : client_id,
+            "client_secret" :self._client_secret,
+            "resource" : resource
         }
-        try:
-            data = requests.post(token_url, data=body, headers=headers)
-            token = data.json()
-            return token
-        except:
-            raise Exception("Could not retrieve token")
+        token_data = requests.post(spo_token_url, headers=headers,data=body)
+        return token_data.json()["access_token"]
         
             
             
         
-        
+    def get_spo_oauth_token(self,client_id, client_secret,app_identifier, spo_base_url):
+        spo_token_url = "https://accounts.accesscontrol.windows.net/"+app_identifier+"/tokens/oauth/2"
+        default_principal_id = "00000003-0000-0ff1-ce00-000000000000"
+        resource = default_principal_id + "/" + spo_base_url + "@" + app_identifier
+        client_id = client_id + "@" + app_identifier
+        headers = {
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        }
+        body = {
+            "grant_type" : "client_credentials",
+            "client_id" : client_id,
+            "client_secret" : client_secret,
+            "resource" : resource
+        }
+        print(body)
+        token_data = requests.post(spo_token_url, headers=headers,data=body)
+        return token_data.json()["access_token"]
     
