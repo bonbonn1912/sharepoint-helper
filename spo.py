89414f2ac2592fbed7863801de3334f67fb56446
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2022-10-19 14:38:48 +0200

    started upload files

--- spo.py
+++ spo.py
@@ -1,4 +1,3 @@
-from email import header
 import requests
 
 class SharePointHelper():
@@ -11,13 +10,14 @@ class SharePointHelper():
     _token_url = "https://accounts.accesscontrol.windows.net"
     _default_principal_audience_id = "00000003–0000–0ff1-ce00–000000000000"
     
-    def __init__(self, tenant, _client_id, _client_secret, _tenant_id):
+    def __init__(self, tenant, _client_id, _client_secret, _tenant_id, init_oauth2_token = True):
         self._tenant = tenant
         self._client_id = _client_id
         self._client_secret = _client_secret
         self._tenant_id = _tenant_id
-        self._base_url = f"https://www.{self._tenant}.sharepoint.com/"
+        self._base_url = f"https://{tenant}.sharepoint.com"
         self._base = f"{tenant}.sharepoint.com"
+        self._oauth2_token = self.get_oauth2_token() if init_oauth2_token else None
     
     def get_oauth2_token(self):
         spo_token_url = f"https://accounts.accesscontrol.windows.net/{self._tenant_id}/tokens/oauth/2"
@@ -35,25 +35,30 @@ class SharePointHelper():
         }
         token_data = requests.post(spo_token_url, headers=headers,data=body)
         return token_data.json()["access_token"]
-        
-            
-            
-        
-    def get_spo_oauth_token(self,client_id, client_secret,app_identifier, spo_base_url):
-        spo_token_url = "https://accounts.accesscontrol.windows.net/"+app_identifier+"/tokens/oauth/2"
-        default_principal_id = "00000003-0000-0ff1-ce00-000000000000"
-        resource = default_principal_id + "/" + spo_base_url + "@" + app_identifier
-        client_id = client_id + "@" + app_identifier
-        headers = {
-            'Content-Type' : 'application/x-www-form-urlencoded'
-        }
-        body = {
-            "grant_type" : "client_credentials",
-            "client_id" : client_id,
-            "client_secret" : client_secret,
-            "resource" : resource
-        }
-        print(body)
-        token_data = requests.post(spo_token_url, headers=headers,data=body)
-        return token_data.json()["access_token"]
+    
+    def upload_file(self,site,spo_path,file_path, file_name = None):
+        data = open(file_path).read()
+        spo_endpoint = f"{self._base_url}/sites/{site}/_api/web/getFolderByServerRelativeUrl('{spo_path}')/files/add(url='{file_name}', overwrite=True)"
+        self.send_request(spo_endpoint, "POST",data, None, self._oauth2_token)
+        #
+    def send_request(self, url, method='GET',body=None,headers=None, token=None):
+        if token == None and self._oauth2_token is None:
+            raise Exception("Please Provide a valid token")
+        else:
+            headers={
+            'Authorization': 'Bearer ' + self._oauth2_token if self._oauth2_token is not None else token,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            }
+        try:
+            response = requests.request(method, url, headers=headers, data=body)
+            print(headers)
+            print(url)
+           # response = requests.post(url, data=body, headers=headers)
+            print(response.json())
+            return response.json()
+        except:
+            print("hallo")
+          #  raise Exception("Could not send request. Please check your Input Parameters")
+
     
